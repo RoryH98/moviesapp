875f6bb916a7f561fd5b962b977ceaf8162d9452
Rory Harford <RoryHarford@live.ie>
2018-02-12 15:02:21 +0000

    Version-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+ controller :sessions do
+	get 'login' => :new
+	post 'login' => :create
+	get 'logout' => :destroy
+	post 'logout' => :destroy
+	end
+	
+
+  resources :users
   resources :genres
   resources :movies
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
